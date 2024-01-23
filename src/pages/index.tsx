6aa0ccf7cda50ef662bbf66047d0cd5445dff8e2
Rachael Miki Buxton <rachaelbuxton1215@gmail.com>
2024-01-23 03:16:37 -0600

    Updated index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,86 +3,8 @@ import type { HeadFC, PageProps } from "gatsby";
 import MyCalendar from "../components/Calendar/Calendar";
 import * as Style from "./global.module.css";
 import StreamingSchedule from "../data/StreamingSchedule";
-import { StreamingScheduleType } from "../types/types";
-import Background from "../components/Background/Background";
-import SocialsBar from "../components/SocialsBar/SocialsBar";
-
-//#region styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-const headingAccentStyles = {
-  color: "#663399",
-};
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-};
-const doclistStyles = {
-  paddingLeft: 0,
-};
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-};
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-};
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-};
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-};
-//#endregion
-
-const StreamButton: React.FC<{ item: StreamingScheduleType }> = ({ item }) => {
-  return (
-    <div className={Style.streamButton}>
-      <div className={Style.streamTime}>{`${item.datetime
-        .getHours()
-        .toString()
-        .padStart(2, "0")}:${item.datetime
-        .getMinutes()
-        .toString()
-        .padStart(2, "0")}~`}</div>
-      <div>{item.title}</div>
-    </div>
-  );
-};
+import Layout from "../components/Layout/Layout";
+import StreamButton from "../components/StreamButton/StreamButton";
 
 const contentArr = StreamingSchedule.map((s) => {
   return {
@@ -93,20 +15,15 @@ const contentArr = StreamingSchedule.map((s) => {
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <>
-      <Background />
-      <main className={Style.main}>
-        <header className={Style.header}>
-          <h1 className={Style.title}>カップ麺</h1>
-          <SocialsBar />
-          <hr className={Style.separator} />
-        </header>
+    <Layout>
+      <main>
+        <h1>カップ麺</h1>
         <section className={Style.section}>
-          <h2 className={Style.h2}>🎮ゲーム配信スケジュール🎮</h2>
+          <h2>🎮ゲーム配信スケジュール🎮</h2>
           <MyCalendar contents={contentArr} />
         </section>
       </main>
-    </>
+    </Layout>
   );
 };
 
