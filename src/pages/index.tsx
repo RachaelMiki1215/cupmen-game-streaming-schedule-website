a437286eb66c094694d3e01109bba4682a4982b9
Rachael Miki Buxton <rachaelbuxton1215@gmail.com>
2024-01-24 23:44:36 -0600

    Added About and Resources section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,12 +18,45 @@ const IndexPage: React.FC<PageProps> = () => {
     <Layout>
       <main>
         <h1>カップ麺</h1>
-        <section className={Style.section}>
+        <section>
           <h2>
             <span>🎮ゲーム配信</span>
             <span>スケジュール🎮</span>
           </h2>
           <MyCalendar contents={contentArr} />
+        </section>
+        <section>
+          <h2>💬このウェブサイトについて💬</h2>
+          <p>
+            このウェブサイトは主に<strong>カップ麺（配信者）</strong>
+            の配信スケジュールを共有するためのものです。
+            <br />
+            本件についてのご質問・ご要望等はX（
+            <a href="https://twitter.com/TonkotsuCupMen" target="_blank">
+              @TonkotsuCupMen
+            </a>
+            ）にて受け付けております。
+          </p>
+        </section>
+        <section>
+          <h2>🍂使用素材🍂</h2>
+          <ul>
+            <li>
+              <a href="https://fontawesome.com/">FontAwesome</a> -
+              JavaScriptで使えるアイコンライブラリ。凄く重宝してる✨
+            </li>
+            <li>
+              <a href="https://fonts.google.com/">Google Fonts</a> -
+              <strong>このフォント</strong>（
+              <a href="https://fonts.google.com/specimen/Yomogi">Yomogi</a>
+              ）をウェブで使用するために配布してくれてるところ。使いやすい🔧
+            </li>
+            <li>
+              <a href="https://mihifont.netlify.app/">みひらめフォント</a> -{" "}
+              <strong className={Style.mihiFont}>このフォント</strong>
+              を配布してくれてるところ。ありがたい🙏
+            </li>
+          </ul>
         </section>
       </main>
     </Layout>
