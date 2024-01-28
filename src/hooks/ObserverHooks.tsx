ade045795eec3f0f151d0f44127376fb88170230
Rachael Miki Buxton <rachaelbuxton1215@gmail.com>
2024-01-27 22:02:49 -0600

    Created observer hook for scroll section entry effect

---
+++ src/hooks/ObserverHooks.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+const useObserver = (
+  containerRef: any,
+  onIntersect?: () => void,
+  onNotIntersect?: () => void,
+  observerOptions?: object
+) => {
+  const [isIntersecting, setIsIntersecting] = useState<boolean>(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      },
+      observerOptions
+        ? observerOptions
+        : {
+            rootMargin: "-200px",
+          }
+    );
+
+    observer.observe(containerRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isIntersecting) {
+      if (onIntersect) {
+        onIntersect();
+      } else {
+        containerRef.current.childNodes.forEach((ch: any) => {
+          ch.style.transform = "translateX(0%)";
+        });
+      }
+    } else {
+      if (onNotIntersect) {
+        onNotIntersect();
+      } else {
+        containerRef.current.childNodes.forEach((ch: any) => {
+          ch.style.transform = "translateX(100%)";
+        });
+      }
+    }
+  }, [isIntersecting]);
+};
+
+export { useObserver };
