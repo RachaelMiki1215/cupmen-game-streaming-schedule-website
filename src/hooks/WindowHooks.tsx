3315f54d1ff63e488a643b91caf5d8b9f5882193
Rachael Miki Buxton <rachaelbuxton1215@gmail.com>
2024-01-27 21:58:49 -0600

    Exported Window size effect to its own hook

---
+++ src/hooks/WindowHooks.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState<{
+    width: number;
+    height: number;
+  }>({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerWidth,
+      });
+    };
+
+    // Get window size on start
+    handleWindowResize();
+
+    // Add window resize listener
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  useEffect(() => {});
+
+  return windowSize;
+};
+
+export { useWindowSize };
