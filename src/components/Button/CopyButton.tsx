f7fa69b609050830f56fa7cecdf9d1091e5115be
Rachael Miki Buxton <rachaelbuxton1215@gmail.com>
2024-03-12 16:02:39 -0500

    [NEW] Added Toast on Copy click

--- src/components/Button/CopyButton.tsx
+++ src/components/Button/CopyButton.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClipboard } from "@fortawesome/free-solid-svg-icons";
 import * as Styles from "./CopyButton.module.css";
+import { createPortal } from "react-dom";
+import Toast, { NotificationType } from "../Toast/Toast";
 
 interface Props {
   children: React.ReactNode | string;
   buttonClassName?: string;
-  contentsToCopy?: any;
+  contentsToCopy: string;
 }
 
 // TODO: Figure out why this button isn't shrinking along with container size.
@@ -15,25 +17,52 @@ const CopyButton: React.FC<Props> = ({
   buttonClassName,
   contentsToCopy,
 }: Props) => {
+  const [isPageRendered, setIsPageRendered] = useState<boolean>(false);
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsPageRendered(true);
+  }, []);
+
   const copyContent = async () => {
     try {
       await navigator.clipboard.writeText(contentsToCopy);
       console.log("[INFO] Content copied to clipboard.");
+
+      setIsCopied(true);
+
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 3000);
     } catch (err) {
       console.error("Failed to copy: ", err);
     }
   };
 
   return (
-    <button
-      onClick={copyContent}
-      className={`${Styles.button} ${buttonClassName}`}
-    >
-      <div className={Styles.overlay}>
-        <FontAwesomeIcon icon={faClipboard} />
-      </div>
-      <div className={Styles.contentDisplay}>{children}</div>
-    </button>
+    <>
+      <button
+        onClick={copyContent}
+        className={`${Styles.button} ${buttonClassName}`}
+      >
+        <div className={Styles.overlay}>
+          <FontAwesomeIcon icon={faClipboard} />
+        </div>
+        <div className={Styles.contentDisplay}>{children}</div>
+      </button>
+      {isPageRendered &&
+        createPortal(
+          <Toast
+            messageType={NotificationType.Info}
+            toastClassName={`${Styles.toast} ${
+              isCopied ? Styles.show : Styles.hide
+            }`}
+          >
+            クリップボードにコピーされました📋
+          </Toast>,
+          document.getElementById("gatsby-focus-wrapper") as Element
+        )}
+    </>
   );
 };
 
--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,22 +1,82 @@
 import React from "react";
 import * as Styles from "./Toast.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleInfo,
+  faCircleQuestion,
+  faCircleXmark,
+  faTriangleExclamation,
+} from "@fortawesome/free-solid-svg-icons";
 
-// TODO: FInish this up.
+// TODO: Finish this up.
 
 enum NotificationType {
-    Info = 0,
-    Warning = 1,
-    Error = 2
+  Info = 0,
+  Warning = 1,
+  Error = 2,
+  Question = 3,
 }
 
 interface ToastProps {
-    messageType?: NotificationType;
-    children?: React.ReactNode;
+  messageType?: NotificationType;
+  children?: React.ReactNode;
+  toastClassName?: string;
 }
 
-const Toast : React.FC<ToastProps> = ({messageType, children} : ToastProps) => {
+const Toast: React.FC<ToastProps> = ({
+  messageType,
+  children,
+  toastClassName,
+}: ToastProps) => {
+  let messageIcon;
+  switch (messageType) {
+    case NotificationType.Info:
+      messageIcon = (
+        <FontAwesomeIcon
+          icon={faCircleInfo}
+          className={Styles.icon}
+          style={{ color: "#135976" }}
+        />
+      );
+      break;
+    case NotificationType.Warning:
+      messageIcon = (
+        <FontAwesomeIcon
+          icon={faTriangleExclamation}
+          className={Styles.icon}
+          style={{ color: "#E0AC2E" }}
+        />
+      );
+      break;
+    case NotificationType.Error:
+      messageIcon = (
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className={Styles.icon}
+          style={{ color: "#E1615F" }}
+        />
+      );
+      break;
+    case NotificationType.Question:
+      messageIcon = (
+        <FontAwesomeIcon
+          icon={faCircleQuestion}
+          className={Styles.icon}
+          style={{ color: "#8863E0" }}
+        />
+      );
+      break;
+    default:
+      break;
+  }
 
-    return <div>
-        {children}
+  return (
+    <div className={`${Styles.container} ${toastClassName}`}>
+      {messageIcon}
+      {children}
     </div>
-}
+  );
+};
+
+export default Toast;
+export { NotificationType };
