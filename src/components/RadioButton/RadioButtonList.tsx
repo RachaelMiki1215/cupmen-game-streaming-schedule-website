73961c6885191ab796bfd384729521e88685f649
Rachael Miki Buxton <rachaelbuxton1215@gmail.com>
2024-03-13 01:49:28 -0500

    Added Radio Button List component

---
+++ src/components/RadioButton/RadioButtonList.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import * as Styles from "./RadioButtonList.module.css";
+
+interface ItemProps {
+  label: string;
+  checkedOnDefault?: boolean;
+  action?: () => void;
+}
+
+interface Props {
+  items: ItemProps[];
+  id: string;
+  containerClass?: string;
+  activeClass?: string;
+  inactiveClass?: string;
+}
+
+const RadioButtonList: React.FC<Props> = ({
+  items,
+  id,
+  containerClass,
+  activeClass,
+  inactiveClass,
+}: Props) => {
+  const [selectedValue, setSelectedValue] = useState<string>(
+    items.filter((item) => item.checkedOnDefault)[0].label
+  );
+
+  const handleOptionChange = (e: any) => {
+    setSelectedValue(e.target.value);
+  };
+
+  return (
+    <fieldset className={`${Styles.container} ${containerClass}`}>
+      {items.map((item, i) => {
+        return (
+          <label className={Styles.label}>
+            {item.label}
+            <input
+              type="radio"
+              name={id}
+              id={id}
+              value={item.label}
+              className={Styles.radioButton}
+              checked={selectedValue === item.label}
+              onChange={(e) => {
+                handleOptionChange(e);
+                item.action ? item.action() : () => {};
+              }}
+            />
+          </label>
+        );
+      })}
+    </fieldset>
+  );
+};
+
+export default RadioButtonList;
+export { ItemProps };
